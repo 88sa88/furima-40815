e36089b368c2024c0b65f00b490b66fa4e310f9a
88sa88 <yutooo.rikooo7@gmail.com>
2024-07-21 22:25:19 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,16 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :Shipping_day
+
   has_one_attached :image
 
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
