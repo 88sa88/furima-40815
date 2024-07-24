6b48c6059012596a5a2fbdc1b2eb65d0b5015ea0
88sa88 <yutooo.rikooo7@gmail.com>
2024-07-24 12:57:15 +0900

    バリテーションの設定とitemのnewルーティングとコントローラーの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,11 @@ class Item < ApplicationRecord
   validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :image, presence: true
+  validates :price, presence: true , format: { with: /\A\d+\z/}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  #学習メモ実装して確認する※バリテーションで空だとエラーとしているが上記で1を選ぶとメッセージが出るようにしている。上記がエラーになる
+  
 end
