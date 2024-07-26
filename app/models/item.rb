97212851ee0ee754571d2480a4e9805a2204ecdd
88sa88 <yutooo.rikooo7@gmail.com>
2024-07-26 23:18:26 +0900

    itemモデルのfactoryｂotの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  belongs_to :user
   has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name       {'test'}
+    explanation     {'test'}
+    category_id     {2}
+    situation_id    {2}
+    postage_id      {2}
+    prefecture_id   {2}
+    shipping_day_id {2}
+    price           {600}
+    user            {association :user }
+
   end
 end
