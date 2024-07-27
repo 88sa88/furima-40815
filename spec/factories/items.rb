d32eaa0c43a13a227dd242fb1daafda216a3b654
88sa88 <yutooo.rikooo7@gmail.com>
2024-07-27 14:02:04 +0900

    itemのFactoryBotを作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,10 @@ FactoryBot.define do
     prefecture_id   {2}
     shipping_day_id {2}
     price           {600}
-    user            {association :user }
+    association :user
 
+    after(:build) do | item |
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+     end
   end
 end
