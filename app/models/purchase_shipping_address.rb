3dd05e00802a892a0221c8d5ac60bb3820346b31
88sa88 <yutooo.rikooo7@gmail.com>
2024-08-12 19:18:37 +0900

    フォームオブジェクトにバリテーションを追記

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,3 +1,14 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id   numericality: {other_than: 0, message: "can't be blank"}
+    validates :municipality
+    validates :street_address
+    validates :telephone_number        format: {with:  format: { with: /\A0\d{9,10}\z/ } ,message: "is invalid. number digits"}
+    validates :purchase
+  end
 end
