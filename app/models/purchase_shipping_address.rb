de9d510ea1186583282b5df1d9d4f4bcadbd073f
88sa88 <yutooo.rikooo7@gmail.com>
2024-08-12 19:33:26 +0900

    attr_accessorの指定

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,5 +1,6 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :minicipality, :street_address, :building, :telephone_number, :prefecture_id
 
   with_options presence: true do
     validates :user_id
