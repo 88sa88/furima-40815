a5cee22ff10fff207ce459065bfd60d6c9b4fc2f
88sa88 <yutooo.rikooo7@gmail.com>
2024-08-12 22:35:45 +0900

    バリテーションの実装とテーブル保存の記述

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -5,11 +5,16 @@ class PurchaseShippingAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id   numericality: {other_than: 0, message: "can't be blank"}
+    validates :post_code,        format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id ,   numericality: {other_than: 0, message: "can't be blank"}
     validates :municipality
     validates :street_address
-    validates :telephone_number        format: {with:  format: { with: /\A0\d{9,10}\z/ } ,message: "is invalid. number digits"}
+    validates :telephone_number, format: {with:  format: { with: /\A0\d{9,10}\z/ } ,message: "is invalid. number digits"}
     validates :purchase
   end
-end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, street_address: street_address, building: building, telephone_number: telephone_number, purchase_id: purchase_id)
+  end
+ end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   validates :password, format:{ with: VALID_PASSWORD_REGEX}
 
   has_many :items
+  has_many :purchases
 
 
 
