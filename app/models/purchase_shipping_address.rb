f77a02bacad768156149cf4d1f5a790317c7dfea
88sa88 <yutooo.rikooo7@gmail.com>
2024-08-14 17:10:15 +0900

    フォームオブジェクトのテストコードを実装

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -5,8 +5,8 @@ class PurchaseShippingAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code,        format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id ,   numericality: {other_than: 0, message: "can't be blank"}
+    validates :post_code,        format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id ,   numericality: {other_than: 1, message: "can't be blank"}
     validates :municipality
     validates :street_address
     validates :telephone_number, format: { with: /\A0\d{9,10}\z/, message: "is invalid. number digits"}
---
+++ spec/factories/purchase_shipping_addresses.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :purchase_shipping_address do
+    post_code {'123-4567'}
+    prefecture_id { 2 }
+    municipality { 'test' }
+    street_address { 'test' }
+    telephone_number { '09011111111'}
+  end
+end
