e165c2275e642afcbd59e7590bc10ec58d7f2094
88sa88 <yutooo.rikooo7@gmail.com>
2024-08-16 23:26:31 +0900

    購入機能のコード修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,16 +1,17 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :index]
-  before_action :redirect_user, only: [:index]
+  before_action :redirect_user, only: [:index, :create]
   
   def index
+    unless @item.purchase.blank?
+      redirect_to root_path
+    end
     @purchase_shipping_address = PurchaseShippingAddress.new
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_shipping_address_params.merge(user_id: current_user.id, item_id: @item.id))
-    
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_shipping_address_params)
     if @purchase_shipping_address.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
@@ -28,14 +29,11 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_shipping_address_params
-    params.require(:purchase_shipping_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number).merge(token: params[:token])
+    params.require(:purchase_shipping_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
   end
 
   def redirect_user
     @item = Item.find(params[:item_id])
-    unless @item.purchase.blank?
-      redirect_to root_path
-    end
   end
 
 end
