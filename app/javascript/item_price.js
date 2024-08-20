ce736439dafeff5881d572b6921970db329071f8
88sa88 <yutooo.rikooo7@gmail.com>
2024-08-20 22:43:45 +0900

    jsの手数料と利益の計算を変更

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,13 @@
 const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input",()=>{
-    const inputValue = priceInput.value;
+    const inputValue = parseFloat(priceInput.value);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    const taxAmount = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = taxAmount;
     const salesProfit = document.getElementById("profit");
-    salesProfit.innerHTML = Math.floor(inputValue * 0.9)
+    salesProfit.innerHTML = inputValue - taxAmount
   })
 };
 
